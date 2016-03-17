7fed3fca4d11b69429a17920b4e15a76703a88cf
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-03-16 23:18:55 -0500

    list/add toggle added, comments added, cleanup

--- song.js
+++ song.js
@@ -6,11 +6,11 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-// add songs to beginning/ end of string
+// add songs to beginning and end of string
 songs.unshift("Untouchable - by PushaT on the album Darkest Before Dawn");
 songs.push("Paradise - by Big Sean on the album Dark Sky Paradise");
 
-// remove/replace unwanted characters
+// remove and replace unwanted characters
 for (var i = 0; i < songs.length; i++) {
 	songs[i] = songs[i].replace(/>/g, "-")
 					   .replace(/\*/g, "")
@@ -18,26 +18,36 @@ for (var i = 0; i < songs.length; i++) {
 					   .replace(/\(/g, "")
 					   .replace(/@/g, "")
 };
-console.log("songs", songs);
 
+//iterating through songs array, inserting content to DOM
 var songText = document.getElementById("theMeat");
 for (var i = 0; i < songs.length; i++) {
 	songText.innerHTML += "<p>" + songs[i] + "</p>";
 }
 
+//targeting add and list link anchor elements in DOM
+var addLink = document.getElementById("add-music");
+var listLink = document.getElementById("list-music");
 
+//targeting add and list content section elements in DOM
+var addView = document.getElementById("add-view").classList;
+var listView = document.getElementById("list-view").classList;
 
-// var addView = document.getElementById("add-music");
-// var listView = document.getElementById("list-music");
+//toggle classes hidden/visible for add and list links
+//list section is loaded as visible, add section as hidden
+addLink.addEventListener("click", function() {
+	if (addView == "hidden"){
+		addView.remove("hidden"),
+		listView.add("hidden");
+	};
+});
+listLink.addEventListener("click", function() {
+	if (listView == "hidden"){
+		listView.remove("hidden"),
+		addView.add("hidden");
+	};
+});
 
-// listView.addEventListener("click", function() {
-//   listView.classList.add("hidden");
-//   addView.classList.add("hidden");
-
-//   listView.classList.add("visible");
-//   addView.classList.remove("hidden");
-
-// });
 
 
 
