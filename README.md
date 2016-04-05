aa8fc366123a0c9673c47ca48276f88cf8c9640b
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-04-04 23:58:23 -0500

    version 4, json file populating dom

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Music History Part 3
+# Music History Part 4
 
 ## Setup
 
@@ -8,30 +8,27 @@ These commands are a helpful quick start. You may choose to ignore them complete
 
 ```bash
 cd /vagrant/musichistory
-git checkout -b version-3
+git checkout -b version-4
 ```
 
 ### Host machine (students not using Vagrant)
 
 ```bash
 cd ~/workspace/musichistory
-git checkout -b version-3
+git checkout -b version-4
 ```
 
 ## Instructions
 
-Time to make Music History into a single page application. Before you begin please [review the sample code](https://github.com/nashville-software-school/front-end-curriculum/tree/master/15-javascript-103#simple-spa-using-events-and-css) I provided in JavaScript 103 about building a simple SPA.
+### Part One
 
-1. In the navigation bar, make sure you have two links labeled "List Music", and "Add Music".
-1. Add a DOM element that contains some input fields for the user to enter in the name of a song, the artist for the song, and the album. You do not need to enclose them in a `<form>` element because we're not actually submitting this form anywhere.
-1. Add a `<button>` element at the bottom of the input fields labeled "Add".
-1. The input fields and the add button make up the *Add Music View*.
-1. The existing view - the combination of the filter form and the song list - will be referred to as the *List Music View*.
-1. The *Add Music View* should not appear when the user first visits your page. The song list with the corresponding filter form should be visible.
-1. When the user clicks on "Add Music" in the navigation bar, the *List Music View* should be hidden, and the *Add Music View* should be shown ([see example wireframe](https://moqups.com/chortlehoort/1E8LJX7r/p:a0cf17f7b)).
-1. When the user clicks on "List Music" in the navigation bar, the *Add Music View* should be hidden, and the *List Music View* should be shown ([see example wireframe](https://moqups.com/chortlehoort/1E8LJX7r/p:a8d99d401)).
-1. Once the user fills out the song form and clicks the add button, you should collect all values from the input fields, add the song to your array of songs, and update the song list in the DOM.
+1. Read from local JSON file with an XHR.
+1. Loop over results and inject into Music History list view.
+1. Add delete button DOM to each row and, when it is clicked, delete the entire row in the DOM.
 
-## Merging
+### Part Two
 
-Remember to merge the `version-3` branch back into `master` when you're done.
+1. Take your music and split it into two JSON file instead of them all living on one file.
+1. Add a button at the bottom of your music list and label it "More >".
+1. Load the songs from the first list and inject the DOM into the document as you've already done.
+1. When the user clicks that button, load the songs from the second JSON file and append them to the bottom of the existing music, but before the More button.
--- song.js
+++ song.js
@@ -1,32 +1,57 @@
 var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 // add songs to beginning and end of string
-songs.unshift("Untouchable - by PushaT on the album Darkest Before Dawn");
-songs.push("Paradise - by Big Sean on the album Dark Sky Paradise");
+// songs.unshift("Untouchable - by PushaT on the album Darkest Before Dawn");
+// songs.push("Paradise - by Big Sean on the album Dark Sky Paradise");
 
 // remove and replace unwanted characters
-for (var i = 0; i < songs.length; i++) {
-	songs[i] = songs[i].replace(/>/g, "-")
-					   .replace(/\*/g, "")
-					   .replace(/!/g, "")
-					   .replace(/\(/g, "")
-					   .replace(/@/g, "")
+// for (var i = 0; i < songs.length; i++) {
+// 	songs[i] = songs[i].replace(/>/g, "-")
+// 					   .replace(/\*/g, "")
+// 					   .replace(/!/g, "")
+// 					   .replace(/\(/g, "")
+// 					   .replace(/@/g, "")
+// };
+
+function executeThisCodeAfterFileIsLoaded () {
+	var songData = "";
+	var currentSong;
+	var songText = document.getElementById("theMeat");
+
+	var data = JSON.parse(this.responseText);
+	// console.log("songs", data.song[0].artist);
+	for (var i = 0; i < data.song.length; i++) {
+	currentSong = data.song[i];
+	songData += `<div class="songBlock">`;
+	songData += `<h4>${currentSong.title}</h4>`;
+	songData += `<p>${currentSong.artist}</p><p>|</p><p>${currentSong.album}</p><p>|</p><p>${currentSong.genre}</p>`;
+	songData += `</div>`
+	};
+	songText.innerHTML = songData;
 };
+
+var songRequest = new XMLHttpRequest();
+songRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
+songRequest.open("GET", "songs.json");
+songRequest.send();
+
+
+
 
 //iterating through songs array, inserting content to DOM
 var songText = document.getElementById("theMeat");
-var populate = function(){
-	for (var i = 0; i < songs.length; i++) {
-	songText.innerHTML += "<p>" + songs[i] + "</p>";
-	}
-};
-populate();
+// var populate = function(){
+// 	for (var i = 0; i < songs.length; i++) {
+// 	songText.innerHTML += "<p>" + songs[i] + "</p>";
+// 	}
+// };
+// populate();
 
 //targeting add and list link anchor elements in DOM
 var addLink = document.getElementById("add-music");
@@ -71,12 +96,12 @@ addBtn.addEventListener("click", function() {
 	var song = document.getElementById("songName").value;
 	var artist = document.getElementById("artistName").value;
 	var album = document.getElementById("albumName").value;
-	//if statement determines whether all input fields have a value before adding new content to DOM/array
+	//determines whether all input fields have a value before adding new content to DOM/array
 	if (song.length > 0 && artist.length > 0 && album.length > 0) {
 		usrSong.push(song + " - by " + artist + " on the album " + album);
 		addSong(usrSong);
 	} else {
-		alert("You must enter text into all fields to continue");
+		alert("Fill the whole thing out ya dingus!");
 	};
 })
 
